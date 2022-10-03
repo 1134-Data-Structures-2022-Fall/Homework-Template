e5e5c2a7135111c0210a93fb30ea98331369980d
calvinatian <48587962+calvinatian@users.noreply.github.com>
2022-10-03 15:05:06 -0400

    CHG update script to use template files

--- export_autograders.py
+++ export_autograders.py
@@ -1,10 +1,18 @@
-import shutil
+import zipfile
+import pathlib
 
-ZIP_OUTPUT_LOCATION = "autograder_zips/"
+QUESTION_DIRECTORIES = [f"q{i}" for i in range(1, 8)]  # CHANGE: q1-q7
 
-question_directories = [f"q{i}" for i in range(1, 8)] # q1-q7
-for dir in question_directories:
-    zip_name = f"{ZIP_OUTPUT_LOCATION}/hw2{dir}"
-    shutil.make_archive(base_name=zip_name,  # output zip directory and name
-                        format="zip",
-                        root_dir=dir)  # directory to zip
+ZIP_OUTPUT_LOCATION  = "autograder_zips"               # NO CHANGE NEEDED
+TEMPLATE_DIRECTORY   = "template"                      # NO CHANGE NEEDED
+TEMPLATE_FILES       = ["requirements.txt",            # NO CHANGE NEEDED
+                        "run_autograder",
+                        "run_tests.py",
+                        "setup.sh"]
+
+for dir in QUESTION_DIRECTORIES:
+    with zipfile.ZipFile(f"{ZIP_OUTPUT_LOCATION}/hw{dir}.zip", mode="w") as archive:
+        for file in TEMPLATE_FILES:  # Archive template files
+            archive.write(f"{TEMPLATE_DIRECTORY}/{file}", arcname=file)  # Archive test files
+        for file in pathlib.Path(dir).rglob("*.py"):  # Recursively search for python files
+            archive.write(file, arcname=file.relative_to(dir))
