edd86b40538f61d87386bb0e930d70d0098704e8
calvinatian <48587962+calvinatian@users.noreply.github.com>
2022-10-11 14:47:15 -0400

    CHG update export script

--- export_autograders.py
+++ export_autograders.py
@@ -1,8 +1,13 @@
 import zipfile
 import pathlib
 
-QUESTION_DIRECTORIES = [f"q{i}" for i in range(1, 8)]  # CHANGE: q1-q7
+### MODIFY THESE VARIABLES ###
+HOMEWORK_NUMBER = 1  # The homework number (1, 2, 3, etc.)
+FIRST_QUESTION  = 1  # The first question number (1, 2, 3, etc.)
+LAST_QUESTION   = 1  # The last question number (1, 2, 3, etc.)
+##############################
 
+QUESTION_DIRECTORIES = [f"q{i}" for i in range(FIRST_QUESTION, LAST_QUESTION + 1)]  # NO CHANGE NEEDED
 ZIP_OUTPUT_LOCATION  = "autograder_zips"               # NO CHANGE NEEDED
 TEMPLATE_DIRECTORY   = "template"                      # NO CHANGE NEEDED
 TEMPLATE_FILES       = ["requirements.txt",            # NO CHANGE NEEDED
@@ -11,7 +16,7 @@ TEMPLATE_FILES       = ["requirements.txt",            # NO CHANGE NEEDED
                         "setup.sh"]
 
 for dir in QUESTION_DIRECTORIES:
-    with zipfile.ZipFile(f"{ZIP_OUTPUT_LOCATION}/hw{dir}.zip", mode="w") as archive:
+    with zipfile.ZipFile(f"{ZIP_OUTPUT_LOCATION}/hw{HOMEWORK_NUMBER}{dir}.zip", mode="w") as archive:
         for file in TEMPLATE_FILES:  # Archive template files
             archive.write(f"{TEMPLATE_DIRECTORY}/{file}", arcname=file)  # Archive test files
         for file in pathlib.Path(dir).rglob("*.py"):  # Recursively search for python files
